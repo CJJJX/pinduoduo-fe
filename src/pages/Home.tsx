28b324ae4f7ae9efece87b5d22c4e4264378260f
蔡健祥 <13772338+CJJJX@user.noreply.gitee.com>
2025-03-23 00:34:41 +0800

    fix Home.tsx 部署后图片404

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import nwuBuilding from '../src/assets/nwu_building.jfif';
+import nwuLogo from '../src/assets/nwu_logo.png';
 export default function Home() {
     return (
         <>
-        <img src="../src/assets/nwu_building.jfif" style={{width: '100vw'}}></img>
-        <img src="../src/assets/nwu_logo.png" style={{width: '30vw'}}></img>
+        <img src={nwuBuilding} style={{width: '100vw'}}></img>
+        <img src={nwuLogo} style={{width: '30vw'}}></img>
         
         </>
     )
